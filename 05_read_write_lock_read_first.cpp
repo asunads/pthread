0317fd70cddfa8a4576a9b47e53737aa9c752dbf
pengjintao <1272813056@qq.com>
2018-08-03 15:10:19 +0800

    读优先的读写锁实现

---
+++ 05_read_write_lock_read_first.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+int thread_num = 8;
+pthread_mutex_t mtx;
+pthread_cond_t read_cond,write_cond;
+int write_num,read_num;
+
+void read_lock()
+{
+	pthread_mutex_lock(&mtx);
+	read_num++;
+	while(write_num > 0) pthread_cond_wait(&write_cond,&mtx);
+	pthread_mutex_unlock(&mtx);
+	//cout<<"r"<<endl;
+	pthread_mutex_lock(&mtx);
+	read_num--;
+	if(read_num == 0)
+		pthread_cond_broadcast(&read_cond);
+	pthread_mutex_unlock(&mtx);
+}
+void write_lock()
+{
+	pthread_mutex_lock(&mtx);
+	while(read_num > 0) pthread_cond_wait(&read_cond,&mtx);
+	//cout<<"w";
+	pthread_mutex_unlock(&mtx);
+}
+void * worker(void *in)
+{
+	for(int i = 0;i<10000;i++)
+	{
+		if(i % 30 == 0){
+			read_lock();
+		}else{
+			write_lock();
+		}
+	}
+	write_lock();
+	return (void *)0;
+}
+
+void Iph_init()
+{
+	write_num = read_num = 0;
+	pthread_mutex_init(&mtx,0);
+	pthread_cond_init(&read_cond,0);
+	pthread_cond_init(&write_cond,0);
+}
+
+void Iph_finalize()
+{
+	pthread_mutex_destroy(&mtx);
+	pthread_cond_destroy(&read_cond);
+	pthread_cond_destroy(&write_cond);
+}
+int main()
+{
+	Iph_init();
+	pthread_t threads[thread_num];
+	int Iph_arg[thread_num];
+	int p = 0;
+
+	for(int i = 0;i<thread_num;i++){
+		Iph_arg[i] = i;
+		pthread_create(&threads[i],0,worker,&Iph_arg[i]);
+	}
+	for(int i =0;i<thread_num;i++)
+	{
+		pthread_join(threads[i],(void **)&p);
+	}
+
+	Iph_finalize();
+	return 0;
+}
